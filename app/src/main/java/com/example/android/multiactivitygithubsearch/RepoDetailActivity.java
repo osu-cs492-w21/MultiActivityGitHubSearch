00c86249885533690a439327efaefde09c3f892d
Rob Hess <robwhess@gmail.com>
2021-02-04 15:46:35 -0800

    Add code from 2/4 lecture, part 2.

--- app/src/main/java/com/example/android/multiactivitygithubsearch/RepoDetailActivity.java
+++ app/src/main/java/com/example/android/multiactivitygithubsearch/RepoDetailActivity.java
@@ -1,12 +1,17 @@
 package com.example.android.multiactivitygithubsearch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.multiactivitygithubsearch.data.GitHubRepo;
 
@@ -15,6 +20,10 @@ public class RepoDetailActivity extends AppCompatActivity {
 
     private static final String TAG = RepoDetailActivity.class.getSimpleName();
 
+    private Toast errorToast;
+
+    private GitHubRepo repo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +31,7 @@ public class RepoDetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra(EXTRA_GITHUB_REPO)) {
-            GitHubRepo repo = (GitHubRepo) intent.getSerializableExtra(EXTRA_GITHUB_REPO);
+            this.repo = (GitHubRepo)intent.getSerializableExtra(EXTRA_GITHUB_REPO);
             Log.d(TAG, "Got repo with name: " + repo.fullName);
 
             TextView repoNameTV = findViewById(R.id.tv_repo_name);
@@ -39,5 +48,51 @@ public class RepoDetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.repo_detail, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_view_on_web:
+                viewRepoOnWeb();
+                return true;
+            case R.id.action_share:
+                shareRepo();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void viewRepoOnWeb() {
+        if (this.repo != null) {
+            Uri githubRepoUri = Uri.parse(this.repo.htmlUrl);
+            Intent intent = new Intent(Intent.ACTION_VIEW, githubRepoUri);
+            try {
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                if (this.errorToast != null) {
+                    this.errorToast.cancel();
+                }
+                this.errorToast = Toast.makeText(this, "Error...", Toast.LENGTH_LONG);
+                this.errorToast.show();
+            }
+        }
+    }
+
+    private void shareRepo() {
+        if (this.repo != null) {
+            String shareText = getString(
+                    R.string.share_repo_text,
+                    this.repo.fullName,
+                    this.repo.htmlUrl
+            );
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_TEXT, shareText);
+            intent.setType("text/plain");
+
+            Intent chooserIntent = Intent.createChooser(intent, null);
+            startActivity(chooserIntent);
+        }
     }
 }
